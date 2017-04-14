39637b0b90bbfc9cdc697e17ecb09bfa28c1c317
Soudabeh <s_arghi@encs.concordia.ca>
2017-04-13 21:16:20 -0400

    develop q1 and q2

---
+++ LogAnlysis.py
@@ -0,0 +1,43 @@
+import sys, re
+
+#sys.argv=['-q', '1', 'a/iliad', 'a/odyssey']
+#execfile('b/logAnalyser.py')
+
+def main():
+	qno = sys.argv[1] 
+	hostDir1 = sys.argv[2] 
+        hostDir2= sys.argv[3] 
+	hosts = []
+	hosts.append(os.path.basename(hostDir1))
+	hosts.append(os.path.basename(hostDir2))
+
+	if qno == '1':
+		
+		regex ='.*?(?:[a-z][a-z]+).*?((?:[a-z][a-z]+))([\s\S]+[\w\W]+[\d\D])'
+	 	rg = re.compile(regex,re.IGNORECASE|re.DOTALL)
+		ls = sc.textFile(hostDir1 + ',' + hostDir2)
+		maps = ls.map(lambda x: None if rg.match(x) is None else (rg.match(x).group(1), 1))
+		
+		m = maps.filter(lambda x: x is not None and x[0] in hosts)
+		hosts=m.reduceByKey(lambda x,y: x+y)
+		show("Q1: Line counts")
+		hosts.foreach(lambda x: show("	+ " + x[0] +": " + str(x[1])))
+	
+	if qno == '2':
+		
+                regex='.*?(?:[a-z][a-z]+).*?((?:[a-z][a-z]+)).*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?((?:[a-z][a-z]+))'
+		rg = re.compile(regex,re.IGNORECASE|re.DOTALL)
+		ls = sc.textFile(hostDir1 + ',' + hostDir2)
+		maps = ls.map(lambda x: None if rg.match(x) is None else (rg.match(x).group(1), rg.match(x).group(2)))
+		
+		m = maps.filter(lambda x: x is not None and x[0] in hosts and x[1] in 'achille')
+		pairs = m.map(lambda x: (x[0],1))
+		final= pairs.reduceByKey(lambda x,y: x + y)
+		show("Q2: Sessions of user achille")	
+		final.foreach(lambda x: show("	+ " + x[0] + ": " + str(x[1])))   
+
+
+
+	
+if __name__ == "__main__":
+   main() 
